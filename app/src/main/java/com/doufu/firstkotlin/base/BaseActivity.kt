7ffbbd06acd3230fa389c6aa0d634f3f9d642cfc
lixindong <lixindong@retailstore.ai>
2019-08-15 09:43:13 +0800

    modify BaseActivity

--- app/src/main/java/com/doufu/firstkotlin/base/BaseActivity.kt
+++ app/src/main/java/com/doufu/firstkotlin/base/BaseActivity.kt
@@ -1,9 +1,14 @@
 package com.doufu.firstkotlin.base
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.Toast
 import com.classic.common.MultipleStatusView
+import pub.devrel.easypermissions.AppSettingsDialog
 
 import pub.devrel.easypermissions.EasyPermissions
 
@@ -57,10 +62,63 @@ abstract class BaseActivity :AppCompatActivity(),EasyPermissions.PermissionCallb
      */
     abstract fun layoutId(): Int
 
-
+    /**
+     * 当权限申请失败的时候执行的回调
+     *
+     * @param requestCode 权限请求的识别码
+     * @param perms       申请的权限的名字
+     */
     override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        val sb=StringBuffer()
+        for (str in perms){
+            sb.append(str)
+            sb.append("\n")
+        }
+        sb.replace(sb.length-2,sb.length,"")
+        if(EasyPermissions.somePermissionPermanentlyDenied(this,perms)){
+            Toast.makeText(this, "已拒绝权限" + sb + "并不再询问", Toast.LENGTH_SHORT).show()
+            AppSettingsDialog.Builder(this)
+                .setRationale("此功能需要" + sb + "权限，否则无法正常使用，是否打开设置")
+                .setPositiveButton("好")
+                .setNegativeButton("不行")
+                .build()
+                .show()
+
+        }
+
     }
 
     override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
     }
+    /**
+     * 打卡软键盘
+     */
+    fun openKeyBord(mEditText:EditText,mContext:Context){
+        val imm=mContext.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(mEditText,InputMethodManager.RESULT_SHOWN)
+        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED,InputMethodManager.HIDE_IMPLICIT_ONLY)
+    }
+    /**
+     * 关闭软键盘
+     */
+    fun closeKeyBord(mEditText: EditText,mContext: Context){
+        val imm=mContext.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(mEditText.windowToken,0)
+    }
+    /**
+     * 重写要申请权限的Activity或者Fragment的onRequestPermissionsResult()方法，
+     * 在里面调用EasyPermissions.onRequestPermissionsResult()，实现回调。
+     *
+     * @param requestCode  权限请求的识别码
+     * @param permissions  申请的权限
+     * @param grantResults 授权结果
+     */
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode,permissions,grantResults)
+
+    }
+
+
+
 }
